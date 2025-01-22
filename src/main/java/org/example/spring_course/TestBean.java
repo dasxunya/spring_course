ca4d60b80fd564f7b91e0b2118e6ef3e0f473d61
da.shapovalova <dasxunya@gmail.com>
2025-01-22 19:38:02 +0300

    init

---
+++ src/main/java/org/example/spring_course/TestBean.java
@@ -0,0 +1,17 @@
+package org.example.spring_course;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
